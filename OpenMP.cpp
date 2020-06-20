a531337fc38cf30f690fd2e694f1f4c11d4eabc8
farukceylandag <39407440+farukceylandag@users.noreply.github.com>
2020-06-20 13:45:34 +0300

    Update OpenMP.cpp

--- OpenMP.cpp
+++ OpenMP.cpp
@@ -1,133 +1,108 @@
-#include <omp.h> //OpenMP kütüphanesi
-#include <ctime> //Süreleri koşup hesaplatmak için gerekli kütüphane
+#include <omp.h>
+#include <ctime>
 #include <iostream>
 
 using namespace std;
 
-#define N 1000 //NxN matris için gerekli büyüklüğünü tanımlama
-#define thread_num 8 //Thread sayısı tanımlama
+#define N 1000
+#define thread_num omp_get_max_threads() //Thread Sayısı
 
-//Float Matrisler
-float A[N][N];
-float B[N][N];
-float C[N][N];
-
-//Double Matrisler
-double D[N][N];
-double E[N][N];
-double F[N][N];
-
-void matrix(); //Matris Fonk. Referans
-
-void float_matmul(); //Float Seri Matris Çarpım Fonk. Referans
-void float_parallel_matmul(); //Float Paralel Matris Çarpım Fonk. Referans
-
-void double_matmul(); //Double Seri Matris Çarpım Fonk. Referans
-void double_parallel_matmul(); //Double Paralel Matris Çarpım Fonk. Referans
-
-int i, j, k;
-
-int main()
+template <class M>
+class Matrix
 {
-    omp_set_num_threads(thread_num);  //Thread Sayısını Setleme.
-    cout << "\nThread Sayisi = " << omp_get_max_threads() << "\n" << endl; //Setlenen Thread Sayısının Ekrena Yazılması
-    cout << N << "x" << N << " icin;\n----------------" << endl;
-    float_matmul(); //Float Seri Matris Çarpım Fonk. Çağırımı
-    float_parallel_matmul(); //Float Paralel Matris Çarpım Fonk. Çağırımı
-    cout << endl;
-    double_matmul();  //Double Seri Matris Çarpım Fonk. Çağırımı
-    double_parallel_matmul(); //Double Paralel Matris Çarpım Fonk. Çağırımı
-}
+public:
 
-void matrix() { //Matris Fonk.
+	Matrix();
+    void Matmul();
+    void Parallel_Matmul();
+
+private:
+
+    static int i, j, k;
+    static M A[N][N];
+    static M B[N][N];
+    static M C[N][N];
+    M deger = 1.0;
+
+
+};
+
+template <class M> int Matrix<M>::i;
+template <class M> int Matrix<M>::j;
+template <class M> int Matrix<M>::k;
+
+template <class M> M Matrix<M>::A[N][N];
+template <class M> M Matrix<M>::B[N][N];
+template <class M>M Matrix<M>::C[N][N];
+
+template <class M>
+Matrix<M>::Matrix() {
+
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++)
         {
             A[i][j] = 1.0;
             B[i][j] = 1.0;
-            D[i][j] = 1.0;
-            E[i][j] = 1.0;  
+            C[i][j] = 0;
         }
     }
-    
-    /*
-    Burada çarpılacak olan matrislerin içini 1.0 değeriyle dolduruyoruz.
-    */
 }
 
-void float_matmul() { //Float Seri Matris Çarpım Fonk.
-    
-    matrix(); //İçini 1.0 ile doldurduğumuz matris fonk. çağırıyoruz.
-    clock_t start = clock(), finish; //Koşma Süresini başlatıyoruz.
+template <class M>
+void Matrix<M>::Matmul(){
+
+    clock_t start = clock(), end;
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
             for (k = 0; k < N; k++) {
-                C[i][j] += A[i][k] * B[k][j]; //Float için matris çarpımlarını yaptık.
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
-    finish = clock(); //Koşma Süresini Bitiriyoruz.
-    cout << "Float Seri Kosma Suresi = " << float(finish - start) / CLOCKS_PER_SEC << " sn" << endl;
-    //Bitiş süresinden başlangıç süresini çıkartıp float seri koşma süresini ekrana yazdırıyoruz.
+    end = clock();
+
+    if (sizeof(M) == sizeof(float))
+    cout << "Float Seri Kosma Suresi = " << float(end - start) / CLOCKS_PER_SEC << " sn" << endl;
+    else if (sizeof(M) == sizeof(double))
+    cout << "Double Seri Kosma Suresi = " << float(end - start) / CLOCKS_PER_SEC << " sn" << endl;
 }
 
-void float_parallel_matmul() { //Float Paralel Matris Çarpım Fonk.
+template <class M>
+void Matrix<M>::Parallel_Matmul() {
 
-    matrix(); 
-    clock_t pstart = clock(), pend; 
-    //Yine serideki gibi içini doldurduğumuz matrislerimizi çekip paralel koşmak için süremizi başlatıyoruz.
-    #pragma omp parallel for private(i,j,k) shared(A,B,C) //Paralel Koşmak için openMP kodu
-    //Burada shared(A,B,C) = A,B,C matrislerini threadlere paylaştırmaya yarıyo.
-        for (i =0; i < N; i++) {
-            for (j = 0; j < N ; j++) {
-                for (k = 0; k < N; k++) {
+    clock_t start = clock(), end;
+    #pragma omp parallel for private(i,j,k) shared(A,B,C)
+        for (i = 0 ; i < N ; i++) {
+            for (j =  0 ; j < N ; j++) {
+                for (k = 0 ; k < N ; k++) {
                     C[i][j] += A[i][k] * B[k][j];
                 }
             }
         }
+ 
+    end = clock();
 
-    pend = clock();
-
-   cout << "Float Paralel Kosma Suresi = " << (float(pend - pstart) / (CLOCKS_PER_SEC*thread_num)) << " sn" << endl;
-    /*
-    Serideki gibi yine koşma süresini hesapladık.Burada seriden tek farkı koşma süresini thread sayısına bölüp
-    birbiriyle paralel koşan threadlerden bir tanesinin süresini erana yazdırmak.
-   */
+    if (sizeof(M) == sizeof(float))
+        cout << "Float Paralel Kosma Suresi(Tek Thread) = " << float(end - start) / (CLOCKS_PER_SEC*thread_num) << " sn" << endl;
+    else if (sizeof(M) == sizeof(double))
+        cout << "Double Paralel Kosma Suresi(Tek Thread) = " << float(end - start) / (CLOCKS_PER_SEC*thread_num) << " sn" << endl;
 }
 
-
-void double_matmul() { //Double Seri Matris Çarpım Fonk.
+int main(){
     
-    //Float Seriyle aynı.Tek fark bu sefer yıkarıda tanımladığımız double matrisleri kullanıyoruz.
-    matrix();
-    clock_t start = clock(), finish;
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            for (k = 0; k < N; k++) {
-                F[i][j] += D[i][k] * E[k][j];
-            }
-        }
-    }
-    finish = clock();
-    cout << "Double Seri Kosma Suresi = " << float(finish - start) / CLOCKS_PER_SEC << " sn" << endl;
-}
+    omp_set_num_threads(thread_num);
+    cout << "\nThread Sayisi = " << omp_get_max_threads()<< "\n" << endl;
+    cout << N << "x" << N << " icin;\n----------------" << endl;
 
-void double_parallel_matmul() { //Double Paralel Matris Çarpım Fonk.
-    
-    //Float Paralel aynı.Tek fark bu sefer yıkarıda tanımladığımız double matrisleri kullanıyoruz.
-    matrix();
-    clock_t pstart = clock(), pend;
-    #pragma omp parallel for private(i,j,k) shared(D,E,F) 
-    //Bu sefer paylaşılan matrisler double veri tipli D , E, F matrisleri
-        for (i = 0; i < N; i++) {
-            for (j = 0; j < N; j++) {
-                for (k = 0; k < N; k++) {
-                    F[i][j] += D[i][k] * E[k][j];   
-                }
-            }
-        }
+    Matrix<float>* fm = new Matrix<float>(); //Float Matrix
+    fm->Matmul();
+    fm->Parallel_Matmul();
+   
+    cout << endl;
 
-    pend = clock();
+    Matrix<double>* dm = new Matrix<double>(); //Double Matrix
+    dm->Matmul();
+    dm->Parallel_Matmul();
 
-    cout << "Double Paralel Kosma Suresi = " << (float(pend - pstart) / (CLOCKS_PER_SEC* thread_num)) << " sn" << endl;
+    return 0;
 }
